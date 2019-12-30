3482db1f7178c9d702d7828c379569e22780674e
edwwlui <rewq142857@hotmail.com>
2019-12-30 17:30:17 +0800

    2

--- hist.py
+++ hist.py
@@ -1,11 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-name="informal_sent_len"
-#single col of values
-data=pd.read_csv(name+".txt")
-data.plot(kind='hist',range=[0,40],bins=20)
-plt.xlabel('Sentence Length')
-plt.title(name,fontsize=15)
-fig = plt.figure(figsize=(4,3))
-fig.savefig(name+'.png', dpi = 300)
-plt.show()
+import sys
+
+def main(argv1):
+    name=argv1
+    #single col of values
+
+    data=pd.read_csv(name,header=None)
+    #data.plot(kind='hist',range=[0,40],bins=20)
+    data.plot(kind='hist')
+    #plt.xlabel('Length')
+    plt.title(name,fontsize=15)
+    plt.show()
+    #fig = plt.figure(figsize=(4,3))
+    #fig.savefig(name+'.png', dpi = 300)
+
+if __name__=='__main__':
+    main(sys.argv[1])
